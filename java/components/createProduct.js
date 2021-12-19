45ec8624b0ecc51685e2b1312b1a778345d9ae9c
Mats Lian Krane <mats_krane@hotmail.com>
2021-12-19 22:26:14 +0100

    last

--- java/components/createProduct.js
+++
@@ -1,8 +0,0 @@
-
-
-
-const form = document.querySelector("form");
-const name = document.querySelector("#name");
-const price = document.querySelector("#price");
-const description = document.querySelector("#description");
-const message = document.querySelector(".message-container");
